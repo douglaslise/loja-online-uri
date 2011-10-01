704870a201b6bba60dfd2a05b171da24cb4a3678
unknown <Aluno@.(none)>
2011-10-01 18:01:59 -0300

    Criado cadastro basico de produtos

---
+++ app/models/produto.rb
@@ -0,0 +1,7 @@
+class Produto < ActiveRecord::Base
+
+  def campo_imagem=(img)
+    self.imagem = img.read
+    self.tipo_imagem = img.content_type.chomp
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loja::Application.config.secret_token = 'd8c48763ea8ee05db925a5e76b0d5409793b2827c096251225c3713796fbf0d4553f9c62eeada942ae69e26bbaba5d9bc1e7ffc02bd46934d2b6bfab3a8a8327'

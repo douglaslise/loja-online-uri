e169288c9091223cd4d8de8705c7992277306b44
Douglas <douglaslise@gmail.com>
2011-10-03 19:20:26 -0300

    Aplicacao criada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loja::Application.config.secret_token = '972f7ce6846000935c479fd0af9a3ff9227ba65b3bbb01567d244d4e9ad4c74465fbe03c36f0f113ed3fd35cde50d8f0d37b82c9b50d45036ad22c38b2ab204e'

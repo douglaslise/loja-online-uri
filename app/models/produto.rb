7be71d051eaea9223d94906aab32575e91975619
Douglas <douglaslise@gmail.com>
2011-10-08 15:39:46 -0300

    adicionadas validacoes

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,4 +1,17 @@
+# encoding: utf-8
 class Produto < ActiveRecord::Base
+
+  validates :titulo, :presence => true,
+    :uniqueness => 
+    {:message => 'já utilizado em outro produto'}
+  validates :descricao, :presence => true
+  validates :preco, :numericality => {
+    :greater_than => 0.0
+  }
+  validates :tipo_imagem, :format => {
+    :with => /^image/,
+    :message => 'não é uma imagem'
+  }
 
   def campo_imagem=(img)
     self.imagem = img.read
